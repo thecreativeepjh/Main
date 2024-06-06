a4e153670b51a320ee856768e8cdd6528e199448
thecreativeepjh <158761944+thecreativeepjh@users.noreply.github.com>
2024-06-05 21:53:57 -0500

    Update projecti.js

--- projecti.js
+++ projecti.js
@@ -21,17 +21,17 @@ function openPage(pageName,elmnt,color) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
-document.getElementById('comand').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form from submitting the traditional way
-
-    // Get the answer from the input field
-    const comand = document.getElementById('comand');
-    const pasward = document.getElementById('pasward');
-
+function input_out() {
+    let comandin = document.getElementById('comand');
+    let comand = comandin.value;
+    let paswardin = document.getElementById('pasward');
+    let pasward = paswardin.value;
+    window.alert(pasward);
     // Check if the answer is correct
     const cpas = "1234";
-    const white = "text white"
+    const white = "text white";
     if (pasward === cpas) {
+        window.alert('hi');
         if (comand === white) {
             document.getElementById('alltext').style.color = "black";
         } else {
@@ -40,4 +40,5 @@ document.getElementById('comand').addEventListener('submit', function(event) {
     } else {
         document.body.className = 'incorrect'; // Change background color to red
     }
-});
+    }
+}
